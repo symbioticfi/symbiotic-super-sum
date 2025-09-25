8608d0a87d774f36e2776e5b2f0ac000944d9efd
oxsteins <oxsteins@gmail.com>
2025-09-25 15:53:20 +0530

    feat: update relay image and go client to latest
    Signed-off-by: oxsteins <oxsteins@gmail.com>

--- generate_network.sh
+++ generate_network.sh
@@ -7,7 +7,7 @@
 set -e
 
 # Define the image tag for the relay service, that the current sum node is compatible with
-RELAY_IMAGE_TAG="0.2.1-20250916142614-eb84d4ec6c54"
+RELAY_IMAGE_TAG="0.2.1-20250925064417-64a0b7ce372a"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
