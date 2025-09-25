77417c1ee282e0c4ca1baabef29797df28782d5c
Andrey <andrey@symbiotic.fi>
2025-09-25 13:51:48 +0400

    chore: sync relay

--- generate_network.sh
+++ generate_network.sh
@@ -7,7 +7,7 @@
 set -e
 
 # Define the image tag for the relay service, that the current sum node is compatible with
-RELAY_IMAGE_TAG="0.2.1-20250916142614-eb84d4ec6c54"
+RELAY_IMAGE_TAG="0.2.1-20250923125502-d4168ab23eaf"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
