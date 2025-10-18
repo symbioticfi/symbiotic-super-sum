bcaabc12c168f4c7c388405dd9a85793f2de7248
oxsteins <oxsteins@gmail.com>
2025-10-18 14:17:10 +0530

    feat: update config
    Signed-off-by: oxsteins <oxsteins@gmail.com>

--- network-scripts/sidecar-start.sh
+++ network-scripts/sidecar-start.sh
@@ -9,20 +9,39 @@ DRIVER_ADDRESS=$(jq -r '.driver.addr' /deploy-data/relay_contracts.json)
 echo "Driver address from relay_contracts.json: $DRIVER_ADDRESS"
 
 cat > /tmp/sidecar.yaml << EOFCONFIG
+# Logging
+log:
+  level: "debug"
+  mode: "pretty"
+
+# API Server Configuration
+api:
+  listen: ":8080"
+
+# Metrics Configuration
+metrics:
+  pprof: true
+
+# Driver Contract
 driver:
   chain-id: 31337
   address: "$DRIVER_ADDRESS"
-log-level: "debug"
-log-mode: "pretty"
-signer: true
-chains:
-  - "http://anvil:8545"
-  - "http://anvil-settlement:8546"
-bootnodes:
-  - /dns4/relay-sidecar-1/tcp/8880/p2p/16Uiu2HAmFUiPYAJ7bE88Q8d7Kznrw5ifrje2e5QFyt7uFPk2G3iR 
-api-listen: ":8080"
-p2p-listen: "/ip4/0.0.0.0/tcp/8880"
-enable-mdns: true
+
+# P2P Configuration
+p2p:
+  listen: "/ip4/0.0.0.0/tcp/8880"
+  bootnodes:
+    - /dns4/relay-sidecar-1/tcp/8880/p2p/16Uiu2HAmFUiPYAJ7bE88Q8d7Kznrw5ifrje2e5QFyt7uFPk2G3iR
+  dht-mode: "server"
+  mdns: true
+
+# EVM Configuration
+evm:
+  chains:
+    - "http://anvil:8545"
+    - "http://anvil-settlement:8546"
+  max-calls: 30
 EOFCONFIG
+
 
 exec /app/relay_sidecar --config /tmp/sidecar.yaml --secret-keys "$1" --storage-dir "$2"
