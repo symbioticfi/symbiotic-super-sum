ca09be2202471b6b2625410fbf623ab82c7dd7c5
alrxy <georgy@symbiotic.fi>
2025-11-20 14:32:30 +0700

    fix: version of docker

--- generate_network.sh
+++ generate_network.sh
@@ -7,7 +7,7 @@
 set -e
 
 # Define the image tag for the relay service, that the current sum node is compatible with
-RELAY_IMAGE_TAG="0.3.1-20251027073813-970b1bfc39c5"
+RELAY_IMAGE_TAG="0.3.1-20251118101405-ac2d86b90063"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
